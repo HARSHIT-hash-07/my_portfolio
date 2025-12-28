94c7cdfd5359ba7d279fcc1812f02e99a263d1a1
HARSHIT-hash-07 <vaghamshiharshit@gmail.com>
2025-12-28 19:37:57 +0530

    Fix: Removed broken Projects route to restore app stability

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,7 +5,6 @@ import { Toaster } from "@/components/ui/toaster";
 import { Navigation } from "@/components/Navigation";
 import { Footer } from "@/components/Footer";
 import Home from "@/pages/Home";
-import Projects from "@/pages/Projects";
 import About from "@/pages/About";
 import Contact from "@/pages/Contact";
 import NotFound from "@/pages/not-found";
@@ -14,7 +13,6 @@ function Router() {
   return (
     <Switch>
       <Route path="/" component={Home} />
-      <Route path="/projects" component={Projects} />
       <Route path="/about" component={About} />
       <Route path="/contact" component={Contact} />
       <Route component={NotFound} />
